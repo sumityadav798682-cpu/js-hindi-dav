bfbcbebd6c5982962f3192b2bb6323a73d2818b9
sumityadav798682-cpu <sumityadav798682@gmail.com>
2026-07-05 06:23:15 +0000

    variable.js

---
+++ variable.js
@@ -0,0 +1,11 @@
+const accountId=12500066
+let accountEmail="Sumit@gmail.com"
+var accountPassword="2314244"
+let accountState;
+accountCity="jalandhar"
+/*Prefer not to use var
+because of issue in block scope and function scope
+*/
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountState,accountCity])
+
