6fd67b716edde0ba8992aaa0b780310aad5e48a5
sumityadav798682-cpu <sumityadav798682@gmail.com>
2026-07-05 06:43:43 +0000

    datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+"Use strict";// treat all js code s newer version
+// alert (3+3) //we are using nodejs,not browser
+console.log(3
+    +3
+)// code readability should be high
+console.log("sumit")
+let name="sumit"
+let age=18
+let isLoggedIn=false
+let state;
+//number=>2 to power 53
+//big int
+//boolean =>true/false
+//null=>standalone value
+//undefined=>undefined
+//sysbol=>unique
+console.log(typeof "sumit"); //string
+console.log(typeof undefined);// undefined
+console.log(typeof null); //object
