dce58c2aa0a4afdd4385b91d4a5fa31fe7978573
sumityadav798682-cpu <sumityadav798682@gmail.com>
2026-07-06 06:06:40 +0000

    datatypeconversion.js

---
+++ datatypeconversion.js
@@ -0,0 +1,23 @@
+let num="123"
+console.log(num)
+console.log(typeof num)
+let num1=Number(num)
+console.log(num1)
+console.log(typeof num1)
+// "33" => 33
+//"33abc"=>NaN but datatype is number
+// true =>1; false=>0
+let loggedIn=1
+let booleanLoggedin=Boolean(loggedIn)
+console.log(booleanLoggedin)
+let name="rakesh"
+let booleanName=Boolean(name)
+console.log(booleanName)
+//"" =>false
+// "rakesh"=>true
+//0 =>false
+//1 =>true
+let number=123
+let stringnum=String(number)
+console.log(stringnum)
+console.log(typeof stringnum)
